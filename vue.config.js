dc9ce9d7fc94bd2adef762699d18d685caebbb6f
nhutvm <vmnhut1996@gmail.com>
2020-10-26 11:13:04 +0700

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  runtimeCompiler: true,
-  devServer: {
-    compress: true,
-    disableHostCheck: true
-  },
   publicPath: process.env.NODE_ENV === 'production' ? '/goal-management-web/' : '',
 
 };
