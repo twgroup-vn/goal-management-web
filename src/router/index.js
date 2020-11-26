6801f7953551310b6d299ec7b9cacfe938d70f8f
nhutvm <vmnhut1996@gmail.com>
2020-11-26 09:32:30 +0700

    deploy commit

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: "/goal-management-web/",
   routes,
   scrollBehavior() {
     return { x: 0, y: 0 };
