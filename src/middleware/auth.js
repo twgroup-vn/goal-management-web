ad826c15d51e6d58246b4e2b006384433f371a2e
nhutvm <vmnhut1996@gmail.com>
2020-11-27 14:54:16 +0700

    update auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,6 +6,7 @@ export default async function auth(to, from, next) {
   const search = window.location.search;
   if(search.includes("refreshtoken")){
     let refreshToken = search.slice(14);
+    localStorage.setItem("TWG_HR_token", refreshToken);
     let authorize = await store.dispatch('$_loginPage/authorizeHRLogin', refreshToken);
     Vue.prototype.$http.defaults.headers.common['Authorization'] = `Bearer ${authorize}`;
     let getUserInfoFromHRPort = await store.dispatch('$_loginPage/getUserInfoFromHRPort', refreshToken);
@@ -21,23 +22,30 @@ export default async function auth(to, from, next) {
   if (!localStorage.getItem('token') && !state.state.token) {
     return next('/login');
   }
-  else if (localStorage.getItem('token')) {
-    var currentTime = new Date().getTime();
-    var expireTime = localStorage.getItem('expireTime');
-    if (expireTime - currentTime < 1000) {
-      await store.dispatch('$_loginPage/refreshToken');
+  else if (localStorage.getItem('token') && localStorage.getItem("TWG_HR_token")) {
+    let success = await store.dispatch('$_loginPage/authorizeHRLogin', localStorage.getItem("TWG_HR_token"));
+    if(success && success != ""){
+      var currentTime = new Date().getTime();
+      var expireTime = localStorage.getItem('expireTime');
+      if (expireTime - currentTime < 1000) {
+        await store.dispatch('$_loginPage/refreshToken');
+      }
+      else {
+        await store.dispatch('$_loginPage/getCurrentUser');
+      }
+      Vue.prototype.$http.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+      if(localStorage.getItem('companyId')){
+        await store.dispatch('$_loginPage/getCompanyDetails', localStorage.getItem('companyId'));
+        Vue.prototype.$http.defaults.headers.common['CompanyId'] = `${localStorage.getItem('companyId')}`;
+        return next();
+      }
+      else
+      {
+        return next('/login');
+      }
     }
-    else {
-      await store.dispatch('$_loginPage/getCurrentUser');
-    }
-    Vue.prototype.$http.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
-    if(localStorage.getItem('companyId')){
-      await store.dispatch('$_loginPage/getCompanyDetails', localStorage.getItem('companyId'));
-      Vue.prototype.$http.defaults.headers.common['CompanyId'] = `${localStorage.getItem('companyId')}`;
-      return next();
-    }
-    else
-    {
+    else{
+      localStorage.clear();
       return next('/login');
     }
   }
