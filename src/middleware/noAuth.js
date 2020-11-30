a1ecc164929ea53b1ead56a901a6bd7d3790d2c6
nhutvm <vmnhut1996@gmail.com>
2020-11-30 10:08:45 +0700

    fixed middleware

--- src/middleware/noAuth.js
+++ src/middleware/noAuth.js
@@ -1,7 +1,7 @@
 import store from '../store'
 import commonData from '../utils/common-data';
 export default async function noAuth(to, from, next) {
- if (localStorage.getItem('token')) {
+ if (localStorage.getItem('token') && localStorage.getItem("TWG_HR_token")) {
    await store.dispatch('$_loginPage/getCurrentUser');
    return next();
  }
